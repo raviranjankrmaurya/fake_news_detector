47b07db06bf1f981434bf6a7e77212c9f4775bbc
Raviranjan Kumar <raviranjankrmaurya@gmail.com>
2026-06-18 05:09:59 +0000

    add config with api key

---
+++ config.js
@@ -0,0 +1,6 @@
+// =============================================
+//   config.js — API Keys (NEVER commit this!)
+//   Yeh file .gitignore mein add karo
+// =============================================
+
+window.__GROQ_API_KEY__ = "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";
