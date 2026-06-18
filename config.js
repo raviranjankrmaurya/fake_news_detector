00b09ce951a2b47ef4da033c0cb5ac35000a3797
Raviranjan Kumar <raviranjankrmaurya@gmail.com>
2026-06-18 05:13:00 +0000

    remove config from tracking

--- config.js
+++
@@ -1,6 +0,0 @@
-// =============================================
-//   config.js — API Keys (NEVER commit this!)
-//   Yeh file .gitignore mein add karo
-// =============================================
-
-window.__GROQ_API_KEY__ = "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";
