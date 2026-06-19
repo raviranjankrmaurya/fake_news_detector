0236b5a3188c71246d8e3cbefd35374f40750f0a
Raviranjan Kumar <raviranjankrmaurya@gmail.com>
2026-06-19 09:01:13 +0000

    Complate project

--- config.example.js
+++
@@ -1,9 +0,0 @@
-// =============================================
-//   config.example.js — Template file
-//   1. Is file ko copy karo → config.js
-//   2. Apni Groq API key paste karo
-//   3. config.js ko .gitignore mein rakho
-// =============================================
-
-window.__GROQ_API_KEY__ = "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";
-// Groq free API key: https://console.groq.com
